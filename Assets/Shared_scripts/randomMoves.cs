42539c49fa222c2efc78d08a3a59e61c236fbb9a
CowTard <ei12109@fe.up.pt>
2016-06-29 13:42:43 +0100

    3rd level

---
+++ Assets/Shared_scripts/randomMoves.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomMoves : MonoBehaviour {
+
+	[Header("Vertical Movement",order=1)]
+	[Tooltip("Check this box if you want the stone to move vertically")]
+	public bool vertical;
+
+	[Header("Parameters",order=1)]
+	[Tooltip("Tweak parameters")]
+	[Range(0,10)]
+	public float velocity = 1f;
+	[Range(0,30)]
+	public float maxDistance = 1f;
+
+
+	private Vector3 initialPos;
+	private bool onSpot;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = this.transform.position;
+		onSpot = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (distance (initialPos, transform.position) < 1) {
+			onSpot = true;
+		}
+
+		if (vertical) {
+			this.transform.position += new Vector3 (0, velocity * Time.deltaTime, 0);
+		} else {
+			this.transform.position += new Vector3 (0, 0, velocity * Time.deltaTime);
+		}
+
+		if (onSpot && distance (transform.position, initialPos) > maxDistance) {
+			velocity *= -1;
+			onSpot = false;
+		}
+	
+	}
+
+
+	float distance(Vector3 cur, Vector3 dest){
+
+
+		return Mathf.Sqrt (Mathf.Pow(dest.x - cur.x,2) + Mathf.Pow(dest.y - cur.y,2) + Mathf.Pow(dest.z - cur.z,2));
+	}
+}
